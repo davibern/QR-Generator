c0f39b610d816832a0a50ecff6e67ef1812b4b0a
David Bernabé <david.bern.pal@gmail.com>
2023-07-22 13:43:45 +0200

    Add color class to show messages on terminal

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from src.qr_generator import QRCodeGenerator
+from src.color_print import ColorPrint
 import sys
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        sys.stderr.write('\x1b[1;31m' + 'Debes especificar una URL como argumento.' + '\x1b[0m')
+        ColorPrint.print_error('Debes especificar una URL para generar el código QR.')
     else:
         url = sys.argv[1]
         if len(sys.argv) < 3:
             file_name = 'qrcode.png'
             qrcode_gen = QRCodeGenerator(url, file_name)
+            ColorPrint.print_info('Código QR generado con el nombre qrcode.png')
         else:
             file_name = sys.argv[2]
             qrcode_gen = QRCodeGenerator(url, file_name)
-            
+            ColorPrint.print_info(f'Código QR generado con el nombre {file_name}')            
         qrcode_gen.create()
