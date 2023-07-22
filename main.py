92ebb6bfb085d765355a8ed6cf8ce5bbf653df45
David Bernabé <david.bern.pal@gmail.com>
2023-07-22 13:26:46 +0200

    Create file qr_generator and call from main

--- main.py
+++ main.py
@@ -1,30 +1,17 @@
-import qrcode
+from src.qr_generator import QRCodeGenerator
 import sys
-import os
 
-def main():
-    try:
-        # Obtain the url across the argument line program
-        url = sys.argv[1]
-        
-        # Create QRCode object
-        qrcode_object = qrcode.make(url)
-        
-        # Directory to save the qrcode image
-        qr_directory = os.path.join('pics', 'qr')
-        
-        # Create directory if exists
-        os.makedirs(qr_directory, exist_ok = True)
-        
-        # Check if user give a file name
-        file_name = sys.argv[2] if sys.argv[2] is not None else 'qrcode.png'   
-        
-        # Save QRCode
-        image_path = os.path.join(qr_directory, file_name)
-        qrcode_object.save(image_path)
-        
-    except IndexError:
-        sys.stderr.write('\x1b[1;31m' + 'No has especificado página web.' + '\x1b[0m')
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) < 2:
+        sys.stderr.write('\x1b[1;31m' + 'Debes especificar una URL como argumento.' + '\x1b[0m')
+    else:
+        url = sys.argv[1]
+        if len(sys.argv) < 3:
+            file_name = 'qrcode.png'
+            qrcode_gen = QRCodeGenerator(url, file_name)
+        else:
+            file_name = sys.argv[2]
+            qrcode_gen = QRCodeGenerator(url, file_name)
+            
+        qrcode_gen.create()
