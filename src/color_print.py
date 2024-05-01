d9d9fa2f50421748584216bd0748887b8a103028
David Bernabé <david.bern.pal@gmail.com>
2024-05-01 20:53:26 +0200

    Remove whitespaces

--- src/color_print.py
+++ src/color_print.py
@@ -1,16 +1,17 @@
 import sys
 
 # Colored printing functions for strings that use universal ANSI escape sequences.
-# fail: bold red, pass: bold green, warn: bold yellow, 
+# fail: bold red, pass: bold green, warn: bold yellow,
 # info: bold blue, bold: bold white
 # standard: default color text
 
+
 class ColorPrint:
-    
+
     @staticmethod
     def print_error(message, end='\n'):
         sys.stderr.write('\x1b[1;31m' + message.strip() + '\x1b[0m' + end)
-    
+
     @staticmethod
     def print_pass(message, end='\n'):
         sys.stdout.write('\x1b[1;32m' + message.strip() + '\x1b[0m' + end)
@@ -26,7 +27,7 @@ class ColorPrint:
     @staticmethod
     def print_bold(message, end='\n'):
         sys.stdout.write('\x1b[1;37m' + message.strip() + '\x1b[0m' + end)
-        
+
     @staticmethod
     def print_standard(message, end='\n'):
         print(message.strip() + end)
